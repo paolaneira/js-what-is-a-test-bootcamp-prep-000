12882f539698cd4abefe22487f860818b514d3ab
paola1792@gmail.com <paola1792@gmail.com>
2018-06-13 15:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 // write your code below!
-var name = "Joe"
-var height = 74
+var name = "Paola "
+var height = 64
 var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
-
-describe('Name', () => {
-  it('returns "Susan"', () => {
-    expect(index.name).toEqual('Susan')
-  })
-})
-
-describe("Height", () => {
-  it("is less than 40", () => {
-    expect(index.height).toBeLessThan(40)
-  })
-})
-
-describe("Message", () => {
-  it("gives the name and height", () => {
-    expect(index.message).toInclude(index.name)
-    expect(index.message).toInclude(index.height)
-  })
-})
